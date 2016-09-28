4b500020cdeb56f6dc3d9e2553b6bab368aafafa
Joe Balistreri <jdbalistreri@gmail.com>
2016-09-28 00:44:51 -0700

    basic prototype

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 
 	_ "github.com/go-sql-driver/mysql"
+	"golang.org/x/crypto/bcrypt"
 )
 
 var db *sql.DB
@@ -25,6 +26,8 @@ func main() {
 		panic(err.Error())
 	}
 
+	http.HandleFunc("/login", login)
+	http.HandleFunc("/signup", signupPage)
 	http.HandleFunc("/", homepage)
 	log.Fatal(http.ListenAndServe(":3000", nil))
 }
@@ -33,9 +36,64 @@ func homepage(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "index.html")
 }
 
-func login(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
-		http.ServeFile(w, r, "login.html")
+func login(res http.ResponseWriter, req *http.Request) {
+	log.Print("logging in")
+	if req.Method != "POST" {
+		log.Print("not a POST - redirecting")
+		http.ServeFile(res, req, "login.html")
 		return
 	}
+
+	username, password := req.FormValue("username"), req.FormValue("password")
+	var dbUsername, dbPassword string
+
+	select1 := "SELECT username, password FROM users WHERE username=?"
+	err := db.QueryRow(select1, username).Scan(&dbUsername, &dbPassword)
+	if err != nil {
+		log.Print("db error - redirecting")
+		http.Redirect(res, req, "/login", 301)
+		return
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(dbPassword), []byte(password))
+	if err != nil {
+		log.Print("bcrypt error - redirecting")
+		http.Redirect(res, req, "/login", 301)
+		return
+	}
+
+	log.Print("success")
+	res.Write([]byte("Hello " + dbUsername))
+}
+
+func signupPage(res http.ResponseWriter, req *http.Request) {
+	if req.Method != "POST" {
+		http.ServeFile(res, req, "signup.html")
+	}
+
+	username, password := req.FormValue("username"), req.FormValue("password")
+	var user string
+
+	dbErr := db.QueryRow("SELECT username FROM users WHERE username=?", username).Scan(&user)
+
+	switch {
+	case dbErr == sql.ErrNoRows:
+		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+		if err != nil {
+			http.Error(res, "Server error, unable to create your account", 500)
+			return
+		}
+
+		_, err = db.Exec("INSERT INTO users(username, password) VALUES(?, ?)", username, hashedPassword)
+		if err != nil {
+			http.Error(res, "Server error, unable to create your account.", 500)
+			return
+		}
+	case dbErr != nil:
+		http.Error(res, "Server error, unable to create your account", 500)
+		return
+	default:
+		http.Redirect(res, req, "/", 301)
+	}
+
 }
